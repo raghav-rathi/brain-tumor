6d0467a31f22c6877ce010691d635dbe6e5e818f
Abhinav Robinson <abhinavrobinson@gmail.com>
2019-10-16 18:50:01 +0530

    Update index.js

--- rest-api/index.js
+++ rest-api/index.js
@@ -3,14 +3,11 @@ const express = require("express"),
   path = require('path');
 
 const cors = require('cors')
-
 const request = require("request");
-
 const port = 8080;
 const app = express();
 
 app.use(cors())
-
 app.use("/", express.static("static"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -18,10 +15,8 @@ app.use(fileUpload({ useTempFiles: true, tempFileDir: "./tmp/" }));
 
 app.post("/upload", (req, res) => {
   console.log(res.body)
-  
   path_ = `brains${Math.floor(Date.now() / 1000)}`;
   filepath = `../flask/in/${path_}.png`;
-
   req.files.brain.mv(filepath, err => {
     if (err) return res.status(500).send(err);
     request("http://localhost:5000/query?path=" + path_, {json:true}, (err, resp, body) => {
@@ -30,7 +25,6 @@ app.post("/upload", (req, res) => {
       }
       res.sendFile(path.join(__dirname,'../flask/out/',`${body}.png`));
     });
-
   });
 });
 
